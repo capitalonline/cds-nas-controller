18c816368add1db19dccc0cad88a2be543d26d42
Honglin Feng <tifayuki@gmail.com>
2020-01-13 19:19:13 +0800

    init commit
    Signed-off-by: Honglin Feng <tifayuki@gmail.com>

---
+++ cmd/provisioner.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"k8s.io/api/core/v1"
+	storage "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog"
+	"sigs.k8s.io/sig-storage-lib-external-provisioner/controller"
+)
+
+const (
+	provisionerNameKey = "PROVISIONER_NAME"
+	defaultProvisioner = "cds/nas"
+)
+
+type nfsProvisioner struct {
+	client kubernetes.Interface
+}
+
+const (
+	mountPath = "/persistentvolumes"
+)
+
+var _ controller.Provisioner = &nfsProvisioner{}
+
+func (p *nfsProvisioner) Provision(options controller.ProvisionOptions) (*v1.PersistentVolume, error) {
+	if options.PVC.Spec.Selector != nil {
+		return nil, fmt.Errorf("claim Selector is not supported")
+	}
+	klog.V(4).Infof("nfs provisioner: VolumeOptions %+v", options)
+
+	pvcNamespace := options.PVC.Namespace
+	pvcName := options.PVC.Name
+	pvName := strings.Join([]string{pvcNamespace, pvcName, options.PVName}, "-")
+	fullPath := filepath.Join(mountPath, pvName)
+	klog.V(4).Infof("creating path %s", fullPath)
+	if err := os.MkdirAll(fullPath, 0777); err != nil {
+		return nil, errors.New("unable to create directory to provision new pv: " + err.Error())
+	}
+	os.Chmod(fullPath, 0777)
+
+	pvs := v1.PersistentVolumeSource{}
+	nasServer, ok := options.StorageClass.Parameters["server"]
+	if !ok {
+		return nil, errors.New("server must be provided in the storage class parameters")
+	}
+	nasServerPath, ok := options.StorageClass.Parameters["path"]
+	if !ok {
+		nasServerPath = "/"
+	}
+	// use flexvolume driver
+	if options.StorageClass.Provisioner == defaultProvisioner {
+
+		flexNasVers, ok := options.StorageClass.Parameters["vers"]
+		if !ok {
+			flexNasVers = "3"
+		}
+		flexNasOptions, ok := options.StorageClass.Parameters["options"]
+		if !ok {
+			flexNasOptions = "nolock,tcp,noresvport"
+		}
+		pvs.FlexVolume = &v1.FlexPersistentVolumeSource{
+			Driver:   defaultProvisioner,
+			ReadOnly: false,
+			Options: map[string]string{
+				"server":  nasServer,
+				"path":    filepath.Join(nasServerPath, pvName),
+				"vers":    flexNasVers,
+				"mode":    options.StorageClass.Parameters["mode"],
+				"options": flexNasOptions,
+			},
+		}
+	} else {
+		// use nfs driver
+		pvs.NFS = &v1.NFSVolumeSource{
+			Server:   nasServer,
+			Path:     nasServerPath,
+			ReadOnly: false,
+		}
+	}
+
+	// create PersistentVolume object
+	pv := &v1.PersistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: options.PVName,
+		},
+		Spec: v1.PersistentVolumeSpec{
+			PersistentVolumeReclaimPolicy: *options.StorageClass.ReclaimPolicy,
+			AccessModes:                   options.PVC.Spec.AccessModes,
+			MountOptions:                  options.StorageClass.MountOptions,
+			Capacity: v1.ResourceList{
+				v1.ResourceStorage: options.PVC.Spec.Resources.Requests[v1.ResourceStorage],
+			},
+			PersistentVolumeSource: pvs,
+		},
+	}
+	return pv, nil
+}
+
+func (p *nfsProvisioner) Delete(volume *v1.PersistentVolume) error {
+	path := volume.Spec.PersistentVolumeSource.NFS.Path
+	pvName := filepath.Base(path)
+	oldPath := filepath.Join(mountPath, pvName)
+	if _, err := os.Stat(oldPath); os.IsNotExist(err) {
+		klog.Warningf("path %s does not exist, deletion skipped", oldPath)
+		return nil
+	}
+	// Get the storage class for this volume.
+	storageClass, err := p.getClassForVolume(volume)
+	if err != nil {
+		return err
+	}
+	// Determine if the "archiveOnDelete" parameter exists.
+	// If it exists and has a false value, delete the directory.
+	// Otherwise, archive it.
+	archiveOnDelete, exists := storageClass.Parameters["archiveOnDelete"]
+	if exists {
+		archiveBool, err := strconv.ParseBool(archiveOnDelete)
+		if err != nil {
+			return err
+		}
+		if !archiveBool {
+			return os.RemoveAll(oldPath)
+		}
+	}
+
+	archivePath := filepath.Join(mountPath, "archived-"+pvName)
+	klog.V(4).Infof("archiving path %s to %s", oldPath, archivePath)
+	return os.Rename(oldPath, archivePath)
+
+}
+
+// getClassForVolume returns StorageClass
+func (p *nfsProvisioner) getClassForVolume(pv *v1.PersistentVolume) (*storage.StorageClass, error) {
+	if p.client == nil {
+		return nil, fmt.Errorf("Cannot get kube client")
+	}
+	className := GetPersistentVolumeClass(pv)
+	if className == "" {
+		return nil, fmt.Errorf("Volume has no storage class")
+	}
+	class, err := p.client.StorageV1().StorageClasses().Get(className, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return class, nil
+}
+
+// GetPersistentVolumeClass returns StorageClassName.
+func GetPersistentVolumeClass(volume *v1.PersistentVolume) string {
+	// Use beta annotation first
+	if class, found := volume.Annotations[v1.BetaStorageClassAnnotation]; found {
+		return class
+	}
+
+	return volume.Spec.StorageClassName
+}
+
+func main() {
+	flag.Parse()
+	flag.Set("logtostderr", "true")
+
+	provisionerName := os.Getenv(provisionerNameKey)
+	if provisionerName == "" {
+		klog.Infof("env %s is empty, use default value: %s", provisionerNameKey, defaultProvisioner)
+	} else {
+		klog.Infof("provisioner name is %s", provisionerName)
+	}
+
+	// Create an InClusterConfig and use it to create a client for the controller
+	// to use to communicate with Kubernetes
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		klog.Fatalf("Failed to create config: %v", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatalf("Failed to create client: %v", err)
+	}
+
+	// The controller needs to know what the server version is because out-of-tree
+	// provisioners aren't officially supported until 1.5
+	serverVersion, err := clientset.Discovery().ServerVersion()
+	if err != nil {
+		klog.Fatalf("Error getting server version: %v", err)
+	}
+
+	clientNFSProvisioner := &nfsProvisioner{
+		client: clientset,
+	}
+	// Start the provision controller which will dynamically provision efs NFS
+	// PVs
+	pc := controller.NewProvisionController(clientset, provisionerName, clientNFSProvisioner, serverVersion.GitVersion)
+	pc.Run(wait.NeverStop)
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/capitalonline/cds-nas-controller
+
+go 1.13
+
+require (
+	github.com/miekg/dns v1.1.27 // indirect
+	github.com/prometheus/client_golang v1.3.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	k8s.io/api v0.17.0
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.16.4
+	k8s.io/klog v1.0.0
+	k8s.io/utils v0.0.0-20200109141947-94aeca20bf09 // indirect
+	sigs.k8s.io/sig-storage-lib-external-provisioner v4.0.1+incompatible
+)

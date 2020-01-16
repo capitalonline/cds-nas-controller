057364653543a57861b2c498f94d595d82b5ce56
Honglin Feng <tifayuki@gmail.com>
2020-01-16 18:24:15 +0800

    remove folder operations
    Signed-off-by: Honglin Feng <tifayuki@gmail.com>

--- cmd/provisioner.go
+++ cmd/provisioner.go
@@ -6,11 +6,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"k8s.io/api/core/v1"
-	storage "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
@@ -29,10 +27,6 @@ type nasProvisioner struct {
 	client kubernetes.Interface
 }
 
-const (
-	mountPath = "/persistentvolumes"
-)
-
 var _ controller.Provisioner = &nasProvisioner{}
 
 func (p *nasProvisioner) Provision(options controller.ProvisionOptions) (*v1.PersistentVolume, error) {
@@ -44,14 +38,6 @@ func (p *nasProvisioner) Provision(options controller.ProvisionOptions) (*v1.Per
 	pvcNamespace := options.PVC.Namespace
 	pvcName := options.PVC.Name
 	pvName := strings.Join([]string{pvcNamespace, pvcName, options.PVName}, "-")
-	fullPath := filepath.Join(mountPath, pvName)
-	klog.Infof("creating path %s", fullPath)
-	if err := os.MkdirAll(fullPath, 0777); err != nil {
-		return nil, errors.New("unable to create directory to provision new pv: " + err.Error())
-	}
-	if err := os.Chmod(fullPath, 0777); err != nil {
-		klog.Errorf("Failed to change mode of directory %s to 0777", fullPath)
-	}
 
 	pvs := v1.PersistentVolumeSource{}
 	nasServer, ok := options.StorageClass.Parameters["server"]
@@ -102,64 +88,10 @@ func (p *nasProvisioner) Provision(options controller.ProvisionOptions) (*v1.Per
 }
 
 func (p *nasProvisioner) Delete(volume *v1.PersistentVolume) error {
-	path := volume.Spec.PersistentVolumeSource.FlexVolume.Options["path"]
-	pvName := filepath.Base(path)
-	oldPath := filepath.Join(mountPath, pvName)
-	klog.Infof("deleting pv %s on path %s(flexvolume path parameter: %s)", pvName, oldPath, path)
-	if _, err := os.Stat(oldPath); os.IsNotExist(err) {
-		klog.Warningf("path %s does not exist, deletion skipped", oldPath)
-		return nil
-	}
-	// Get the storage class for this volume.
-	storageClass, err := p.getClassForVolume(volume)
-	if err != nil {
-		klog.Errorf("failed to get storage class from volume %s: %s", volume.Name, err)
-		return err
-	}
-	// Determine if the "archiveOnDelete" parameter exists.
-	// If it exists and has a false value, delete the directory.
-	// Otherwise, archive it.
-	archiveOnDelete, exists := storageClass.Parameters["archiveOnDelete"]
-	if exists {
-		archiveBool, err := strconv.ParseBool(archiveOnDelete)
-		if err != nil {
-			return err
-		}
-		if !archiveBool {
-			return os.RemoveAll(oldPath)
-		}
-	}
-
-	archivePath := filepath.Join(mountPath, "archived-"+pvName)
-	klog.Infof("archiving path %s to %s", oldPath, archivePath)
-	return os.Rename(oldPath, archivePath)
+	klog.Infof("persistent volume delete is called: %+v", volume)
+	return nil
 }
 
-// getClassForVolume returns StorageClass
-func (p *nasProvisioner) getClassForVolume(pv *v1.PersistentVolume) (*storage.StorageClass, error) {
-	if p.client == nil {
-		return nil, fmt.Errorf("cannot get kube client")
-	}
-	className := GetPersistentVolumeClass(pv)
-	if className == "" {
-		return nil, fmt.Errorf("volume has no storage class")
-	}
-	class, err := p.client.StorageV1().StorageClasses().Get(className, metav1.GetOptions{})
-	if err != nil {
-		return nil, err
-	}
-	return class, nil
-}
-
-// GetPersistentVolumeClass returns StorageClassName.
-func GetPersistentVolumeClass(volume *v1.PersistentVolume) string {
-	// Use beta annotation first
-	if class, found := volume.Annotations[v1.BetaStorageClassAnnotation]; found {
-		return class
-	}
-
-	return volume.Spec.StorageClassName
-}
 
 func main() {
 	flag.Parse()
